025270b5fe969a38e954b8650e77cae11f2ec14b
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-07 02:23:54 -0800

    task 8

--- models/user.py
+++ models/user.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+""" This module is a subclass of BaseModel
 
+    class: User - inherit from BaseModel class
+
+    attr: email, password, first_name, last_name
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class inherits from BaseModel."""
+    """User class inherits from BaseModel.
+
+        attr: email, password, first_name, last_name
+
+        constructor: __init__, args and kwargs
+    """
 
     email = ""
     password = ""
